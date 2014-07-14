de66efbec3c961f7c6c083219135708d62e13a87
Jozef Butko <jbutko@gmail.com>
2014-07-14 13:55:59 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function bodyHeight() {
+	
+	var w = window,
+	    d = document,
+	    e = d.documentElement,
+	    g = d.getElementsByTagName('body')[0],
+	    x = w.innerWidth || e.clientWidth || g.clientWidth,
+	    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+	if ( x > 1000) {
+		if (window.screenTop && window.screenY) {
+		    g.style.height = 'auto';
+		} else {
+			g.style.height = '100%';
+		}
+	}
+}
+
+bodyHeight();
+
+window.addEventListener('resize', function(event){
+  bodyHeight();
+});
+
+
+
